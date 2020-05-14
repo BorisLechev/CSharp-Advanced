ccfa0bcee1a58138af6029ecb5ac3b0d19a2693a
BorisLechev <b.k.lechev@gmail.com>
2020-05-14 08:53:48 +0300

    Added Tetris with OOP - Part 1

---
+++ C# OOP/Tetris/Tetris/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿namespace Tetris
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public class ScoreManager
+    {
+        private readonly string hignScoreFile;
+
+        public ScoreManager(string hignScoreFile)
+        {
+            this.hignScoreFile = hignScoreFile;
+        }
+
+        public int GetHignScore()
+        {
+            var hignScore = 0;
+
+            if (File.Exists(this.hignScoreFile))
+            {
+                var allScores = File.ReadAllLines(this.hignScoreFile);
+
+                foreach (var score in allScores)
+                {
+                    var match = Regex.Match(score, @" => (?<score>[0-9]+)");
+
+                    hignScore = Math.Max(hignScore, int.Parse(match.Groups["score"].Value));
+                }
+            }
+
+            return hignScore;
+        }
+
+        public void Add(int score)
+        {
+            File.AppendAllLines(this.hignScoreFile, new List<string>
+            {
+                $"[{DateTime.Now.ToString()}] {Environment.UserName} => {score}"
+            });
+        }
+    }
+}
