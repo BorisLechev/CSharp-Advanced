9054249896be7b91be15616d6c71301aa808c22d
BorisLechev <b.k.lechev@gmail.com>
2021-08-02 23:58:41 +0300

    Add aditional solution for RhombusOfStars, Point in Rectangle and Point in Circle

---
+++ C# OOP/1. Working with Abstraction/RhombusOfStars/RhombusOfStars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var drawer = new RhombusAsStringDrawer();
+            Console.WriteLine(drawer.Draw(n));
+        }
+    }
+}
---
+++ C# OOP/1. Working with Abstraction/RhombusOfStars/RhombusOfStars/RhombusAsStringDrawer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RhombusOfStars
+{
+    public class RhombusAsStringDrawer
+    {
+        public string Draw(int countOfStars)
+        {
+            StringBuilder sb = new StringBuilder();
+            DrawTopPart(countOfStars, sb);
+            DrawBottomPart(countOfStars, sb);
+
+            return sb.ToString();
+        }
+
+        private static void DrawTopPart(int countOfStars, StringBuilder sb)
+        {
+            for (int star = 1; star <= countOfStars; star++)
+            {
+                sb.Append(new string(' ', countOfStars - star));
+                DrawLineOfStars(star, sb);
+            }
+        }
+
+        private static void DrawBottomPart(int n, StringBuilder sb)
+        {
+            for (int star = n - 1; star >= 1; star--)
+            {
+                sb.Append(new string(' ', n - star));
+                DrawLineOfStars(star, sb);
+            }
+        }
+
+        private static void DrawLineOfStars(int numberofStars, StringBuilder sb)
+        {
+            for (int star = 1; star <= numberofStars; star++)
+            {
+                sb.Append('*');
+
+                if (star < numberofStars)
+                {
+                    sb.Append(' ');
+                }
+            }
+
+            sb.AppendLine();
+        }
+    }
+}
