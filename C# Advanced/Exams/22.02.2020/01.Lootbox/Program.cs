77b3cd6aa2996fbfc0d38608578f18f4b0f6126c
BorisLechev <b.k.lechev@gmail.com>
2021-08-14 21:15:32 +0300

    Exam 22.02.2020

---
+++ C# Advanced/Exams/22.02.2020/01.Lootbox/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLootBox = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var secondLootBox = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var firstBoxQueue = new Queue<int>(firstLootBox);
+            var secondBoxStack = new Stack<int>(secondLootBox);
+
+            int sumOfClaimedItems = 0;
+
+            while (firstBoxQueue.Any() && secondBoxStack.Any())
+            {
+                var firstBoxItem = firstBoxQueue.Peek();
+                var secondBoxItem = secondBoxStack.Pop();
+
+                var sum = firstBoxItem + secondBoxItem;
+
+                if (sum % 2 == 0)
+                {
+                    sumOfClaimedItems += sum;
+
+                    firstBoxQueue.Dequeue();
+                }
+                else
+                {
+                    firstBoxQueue.Enqueue(secondBoxItem);
+                }
+            }
+
+            if (firstBoxQueue.Any() == false)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondBoxStack.Any() == false)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (sumOfClaimedItems >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sumOfClaimedItems}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sumOfClaimedItems}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/22.02.2020/02.Re-Volt/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace _02.Re_Volt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            var matrix = new char[size, size];
+
+            var playerRow = 0;
+            var playerCol = 0;
+            bool isWon = false;
+
+            InitializeMatrix(matrix, ref playerRow, ref playerCol);
+
+            while (isWon == false && numberOfCommands > 0)
+            {
+                string command = Console.ReadLine();
+
+                matrix[playerRow, playerCol] = '-';
+                numberOfCommands--;
+
+                NextPosition(ref playerRow, ref playerCol, command);
+                ValidatePlayerPosition(matrix, ref playerRow, ref playerCol);
+
+                if (matrix[playerRow, playerCol] == 'F')
+                {
+                    isWon = true;
+                    matrix[playerRow, playerCol] = 'f';
+
+                    break;
+                }
+                else if (matrix[playerRow, playerCol] == 'T')
+                {
+                    PreviousPosition(ref playerRow, ref playerCol, command);
+                }
+                else if (matrix[playerRow, playerCol] == 'B')
+                {
+                    NextPosition(ref playerRow, ref playerCol, command);
+                }
+
+                ValidatePlayerPosition(matrix, ref playerRow, ref playerCol);
+
+                matrix[playerRow, playerCol] = 'f';
+            }
+
+            if (isWon)
+            {
+                Console.WriteLine("Player won!");
+            }
+            else
+            {
+                Console.WriteLine("Player lost!");
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void PreviousPosition(ref int playerRow, ref int playerCol, string command)
+        {
+            switch (command)
+            {
+                case "up":
+                    playerRow++;
+                    break;
+                case "down":
+                    playerRow--;
+                    break;
+                case "left":
+                    playerCol++;
+                    break;
+                case "right":
+                    playerCol--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void ValidatePlayerPosition(char[,] matrix, ref int playerRow, ref int playerCol)
+        {
+            if (playerRow < 0)
+            {
+                playerRow = matrix.GetLength(0) - 1;
+            }
+            if (playerRow >= matrix.GetLength(0))
+            {
+                playerRow = 0;
+            }
+            if (playerCol < 0)
+            {
+                playerCol = matrix.GetLength(1) - 1;
+            }
+            if (playerCol >= matrix.GetLength(1))
+            {
+                playerCol = 0;
+            }
+        }
+
+        private static void NextPosition(ref int playerRow, ref int playerCol, string command)
+        {
+            switch (command)
+            {
+                case "up":
+                    playerRow--;
+                    break;
+                case "down":
+                    playerRow++;
+                    break;
+                case "left":
+                    playerCol--;
+                    break;
+                case "right":
+                    playerCol++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void InitializeMatrix(char[,] matrix, ref int playerRow, ref int playerCol)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+
+                    if (matrix[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+        }
+    }
+}
